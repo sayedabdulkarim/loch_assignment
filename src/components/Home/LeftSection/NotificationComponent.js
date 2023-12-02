e891e56c35cc275548f1e2d90234e8fd3d949a63
unknown <sakarim9124@gmail.com>
2023-12-03 00:47:05 +0530

    updated

--- src/components/Home/LeftSection/NotificationComponent.js
+++ src/components/Home/LeftSection/NotificationComponent.js
@@ -1,9 +1,25 @@
 import React from "react";
 import { HomeBellIcon } from "../../../utils/svgs";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import {
+  Pagination,
+  Navigation,
+  HashNavigation,
+  Autoplay,
+} from "swiper/modules";
 
 const NotificationComponent = () => {
   return (
     <div className="notification_component">
+      {/*  */}
       <div className="left_section">
         <HomeBellIcon />
         <h3 className="first_label">
@@ -14,7 +30,47 @@ const NotificationComponent = () => {
           on-chain or when a dormant whale you care about becomes active.
         </h4>
       </div>
-      <div className="right_section"></div>
+      {/*  */}
+      <div className="right_section">
+        <Swiper
+          hashNavigation={{
+            watchState: false,
+          }}
+          modules={[Pagination, Navigation, HashNavigation, Autoplay]}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          // autoplay={{
+          //   delay: 2500, // Delay between transitions in ms
+          //   disableOnInteraction: false, // To continue autoplay after user interaction
+          //   pauseOnMouseEnter: true,
+          // }}
+          className="portfolio_slide_container"
+          style={
+            {
+              // height: "310px",
+            }
+          }
+        >
+          <div class="swiper-wrapper">
+            <SwiperSlide className="portfolio_slide_item">
+              <>
+                <HomeBellIcon />
+                <p>We’ll be sending notifications to you here</p>
+                <span>hello@gmail.com</span>
+              </>
+            </SwiperSlide>
+            <SwiperSlide className="portfolio_slide_item">
+              <>
+                <HomeBellIcon />
+                <p>12345678</p>
+                <span>hello@gmail.com</span>
+              </>
+            </SwiperSlide>
+          </div>
+        </Swiper>
+      </div>
     </div>
   );
 };
